aab20c9196023c6e57ff1f516de707db6c40307c
Anne Saber <iarabejo3@gmail.com>
2022-04-29 23:33:44 -0500

    Takes updated content and save to IndexDB and gets all content from IndexDB in database.js file.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,27 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const txtEditorDB = await openDB('txtEditor', 1);
+  const transVar = txtEditorDB.transaction('txtEditor', 'readwrite');
+  const storeVar = transVar.objectStore('txtEditor');
+  const request = storeVar.put({ id: 1, value: content });
+  
+  const result = await request;
+  console.log('🚀 - data saved to the database', result.value);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const txtEditorDB = await openDB('txtEditor', 1);
+  const transVar = txtEditorDB.transaction('txtEditor', 'readonly');
+  const storeVar = transVar.objectStore('txtEditor');
+  const request = storeVar.get(1);
+  const result = await request;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+  return result?.value;
+};
 
 initdb();
